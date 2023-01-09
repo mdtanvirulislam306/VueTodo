f1af7963d0a0e2560f0f99d20c693242128231a0
Tanvir <mdtanvirulislam306@gmail.com>
2023-01-09 22:49:35 +0600

    Done

--- src/components/CartComponent.vue
+++ src/components/CartComponent.vue
@@ -6,6 +6,7 @@
           <div class="card-header" v-if="cartHeader">{{ cartHeader }}</div>
           <div class="card-body">
             <form @submit.prevent="addTodo">
+<<<<<<< Updated upstream
               <div class="input-group">
                 <input
                   type="text"
@@ -19,6 +20,14 @@
               </div>
             </form>
             <div class="row mt-3" v-if="todos.length > 0">
+=======
+            <div class="input-group">
+              <input type="text" class="form-control" placeholder="Add Todo " v-model="todo" />
+              <button class="btn btn-primary" type="submit">Add</button>
+            </div>
+        </form>
+            <div class="row mt-3"  v-if="todos.length>0">
+>>>>>>> Stashed changes
               <table class="table table-bordered table-striped">
                 <thead>
                   <tr class="table-primary">
@@ -27,6 +36,7 @@
                   </tr>
                 </thead>
                 <tbody>
+<<<<<<< Updated upstream
                   <tr v-for="(todo, index) in todos" :key="index">
                     <td :class="{ 'text-danger': todo.status }">
                       <del v-if="todo.status">
@@ -45,13 +55,26 @@
                         class="fas fa-trash text-danger mx-2"
                         @click="deleteTodo(todo.id)"
                       ></i>
+=======
+                  <tr v-for="(todo,index) in todos" :key="index">
+                    <td>{{ ++index }}</td>
+                    <td>{{ todo.title }}</td>
+                    <td>
+                      <button class="btn btn-info mx-2" @click="editTodo(index)">Edit</button>
+                      <button class="btn btn-danger" @click="deleteTodo(todo.id)">Delete</button>
+>>>>>>> Stashed changes
                     </td>
                   </tr>
                 </tbody>
               </table>
             </div>
+<<<<<<< Updated upstream
             <div class="row" v-else>
               <p>No Todo found hare...</p>
+=======
+            <div class="row text-center mt-3" v-else>
+              <p>No Todos found...</p>
+>>>>>>> Stashed changes
             </div>
           </div>
         </div>
@@ -71,31 +94,19 @@ export default {
     };
   },
   methods: {
-    addTodo() {
-      const data = {
-        id: uuidv4(),
-        title: this.todo,
-        status: this.isCompelete,
-      };
-      this.todos = [...this.todos, data];
-      this.todo = "";
+    addTodo(){
+    
+        const data = {
+            id:uuidv4(),
+            title: this.todo,
+            status: false
+        }
+        this.todos = [...this.todos,data]
+        this.todo = ''
     },
-    doneTodo(index) {
-      this.todos[index].status = !this.todos[index].status;
-    },
-    editTodo(index) {
-      this.isUpadate = true;
-      this.todo = this.todos[index].title;
-      this.updateIndex = index;
-    },
-    updatedTodo(index) {
-    console.log(this.todos[index])
-      this.todos[index].title = 'test';
-
-    },
-    deleteTodo(index) {
-      this.todos = this.todos.filter((item) => item.id !== index);
-    },
-  },
+    deleteTodo(id){
+        this.todos = this.todos.filter(item =>item.id!==id)
+    }
+  }
 };
 </script>
