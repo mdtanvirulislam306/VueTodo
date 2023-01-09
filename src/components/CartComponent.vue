117db9b00d2ad63db82a9efc540917360511e5cb
tanvir-kholabazaar <mdtanvirulislam306@gmail.com>
2023-01-09 13:25:58 +0600

    update

--- src/components/CartComponent.vue
+++ src/components/CartComponent.vue
@@ -8,29 +8,32 @@
             <form @submit.prevent="addTodo">
             <div class="input-group">
               <input type="text" class="form-control" placeholder="Add Todo " v-model="todo" />
-              <button class="btn btn-primary" type="submit">Add</button>
+              <button class="btn btn-primary" type="submit" v-if="!isUpadate">Add</button>
+              <button class="btn btn-primary" type="submit" @click="updatedTodo(updateIndex)" v-else>Save</button>
             </div>
         </form>
-            <div class="row mt-3">
+            <div class="row mt-3" v-if="todos.length>0">
               <table class="table table-bordered table-striped">
                 <thead>
                   <tr class="table-primary">
-                    <th>sl</th>
                     <th>Todo</th>
                     <th>Action</th>
                   </tr>
                 </thead>
                 <tbody>
-                  <tr v-for="todo,index in todos" :key="index">
-                    <td>{{ ++index }}</td>
-                    <td>{{ todo.title }}</td>
-                    <td>
-                      <button class="btn btn-info mx-2" @click="editTodo(todo.id)">Edit</button>
-                      <button class="btn btn-danger" @click="deleteTodo(todo.id)">Delete</button>
+                  <tr v-for="(todo,index) in todos" :key="index">
+                    <td  :class="{'text-danger': todo.status}"><del v-if="todo.status">{{ todo.title }}</del><span v-else>{{ todo.title }}</span></td>
+                    <td width="200px">
+                      <i class="fas fa-check text-primary mx-2" @click="doneTodo(index)"></i>
+                      <i class="fas fa-pencil text-info mx-2" @click="editTodo(index)"></i>
+                      <i class="fas fa-trash text-danger mx-2" @click="deleteTodo(todo.id)"></i>
                     </td>
                   </tr>
                 </tbody>
               </table>
+            </div>
+            <div class="row" v-else>
+              <p >No Todo found hare...</p>
             </div>
           </div>
         </div>
@@ -46,6 +49,9 @@ export default {
   data() {
     return {
         todo:'',
+        isUpadate:false,
+        updateIndex:'',
+        isCompelete:false,
       todos: []
     };
   },
@@ -54,14 +60,22 @@ export default {
         const data = {
             id:uuidv4(),
             title: this.todo,
-            status: false
+            status: this.isCompelete
         }
         this.todos = [...this.todos,data]
         this.todo = ''
     },
+    doneTodo(index){
+      this.todos[index].status = !this.todos[index].status
+    },
     editTodo(index){
-        const updatedData = this.todos.filter(item =>item.id==index)
+        this.isUpadate = !this.isUpadate
+        this.todo = this.todos[index].title
+        this.updateIndex = index
         
+    },
+    updatedTodo(index){
+      this.todos[index].title = this.todo
     },
     deleteTodo(index){
         this.todos = this.todos.filter(item =>item.id!==index)
